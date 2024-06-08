a2e7fc5a46536788d49e4263e7d44643e6d33840
acon <101356372+acornitum@users.noreply.github.com>
2024-06-08 08:23:35 -0400

    basic text

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex h-screen px-[10vw] py-0">
+        <div className="w-[50vw] flex flex-col justify-center">
+
+          <div className="text-[30px]">
+            <p>CYBERPOLIS </p>
+            
+          </div>
+          
+
+          <div className="mt-4">
+            <p>a hackathon.</p>
+            <p>more info coming soon.</p>
+            
+          </div>
+
+
+
+        </div>
+
+        <div className="">
+
+
+
+        </div>
+
+
+        
+
+
+      </div>
+    </main>
+  );
+}
